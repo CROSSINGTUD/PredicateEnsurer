f977636419582dfac434957dc91ce1180496ed6a
AndreSonntag <Andre-Sonntag@gmx.de>
2019-02-20 15:19:58 +0100

    add PredicateEnsurer maven project
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

---
+++ PredicateEnsurer/src/main/java/de/predicateensurer/CC.java
@@ -0,0 +1,13 @@
+package de.predicateensurer;
+
+public class CC {
+	/**
+	 * This method belongs to the CogniCrypt plugin and inserts a predicate for a certain object.
+	 * @param var - name of the variable
+	 * @param predicate -  predicate for the variable
+	 * @return the return value is not relevant
+	 */
+	public static boolean ensuresPredicate(String var, String predicate) {
+		return true;
+	}
+}
