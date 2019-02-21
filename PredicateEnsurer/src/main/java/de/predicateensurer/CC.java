32cc9d3960164184259b4958f7c41092cbf771a2
AndreSonntag <Andre-Sonntag@gmx.de>
2019-02-21 10:18:47 +0100

    Refactoring
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

--- PredicateEnsurer/src/main/java/de/predicateensurer/CC.java
+++ PredicateEnsurer/src/main/java/de/predicateensurer/CC.java
@@ -2,9 +2,9 @@ package de.predicateensurer;
 
 public class CC {
 	/**
-	 * This method belongs to the CogniCrypt plugin and inserts a predicate for a certain object.
+	 * This method belongs to the CogniCrypt plugin and inserts a predicate for a certain object. 
 	 * @param var - name of the variable
-	 * @param predicate -  predicate for the variable
+	 * @param predicate - predicate for the variable
 	 * @return the return value is not relevant
 	 */
 	public static boolean ensuresPredicate(String var, String predicate) {
