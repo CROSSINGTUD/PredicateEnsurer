41089946809c9ee96066705738040cd1b17e7aa1
AndreSonntag <Andre-Sonntag@gmx.de>
2019-08-19 13:40:45 +0200

    update class name, method parameter, pom
    Signed-off-by: AndreSonntag <Andre-Sonntag@gmx.de>

---
+++ PredicateEnsurer/src/main/java/de/upb/cognicrypt/predicateensurer/Ensurer.java
@@ -0,0 +1,13 @@
+package de.upb.cognicrypt.predicateensurer;
+
+public class Ensurer {
+	/**
+	 * This method belongs to the CogniCrypt plugin and inserts a predicate for a certain object. 
+	 * @param var - name of the variable
+	 * @param predicate - predicate for the variable
+	 * @return the return value is not relevant
+	 */
+	public static boolean ensuresPredicate(Object obj, String predicate) {
+		return true;
+	}
+}
